24589019b444b64e1229a5df4935f72c860cf678
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-09-12 09:53:10 +0200

    Fix test_setup_keywords

--- test/test_setup_keywords.py
+++ test/test_setup_keywords.py
@@ -4,23 +4,23 @@ import sys
 import pytest
 import zetup
 
-import nodely
-
-
-def test_setup_entry_point(node_package):
-    node_package_dir = nodely.NODE_MODULES_DIR / node_package
-    nodely.uninstall(node_package)
-    assert not node_package_dir.exists()
-    nodely.setup_entry_point(dist=None, jsmodules=[node_package])
-    assert node_package_dir.isdir()
-
-
-def test_setup_entry_point_with_wrong_keyword():
-    with pytest.raises(AssertionError):
-        nodely.setup_entry_point(dist=None, keyword='wrong')
+from nodely.setup_keywords import require_node_modules
 
 
 def test_require_node_modules(node_package):
+    node_package_dir = nodely.NODE_MODULES_DIR / node_package
+    nodely.uninstall(node_package)
+    assert not node_package_dir.exists()
+    require_node_modules(dist=None, jsmodules=[node_package])
+    assert node_package_dir.isdir()
+
+
+def test_require_node_modules_with_wrong_keyword():
+    with pytest.raises(AssertionError):
+        require_node_modules(dist=None, keyword='wrong')
+
+
+def test_setup(node_package):
     node_package_dir = nodely.NODE_MODULES_DIR / node_package
     nodely.uninstall(node_package)
     assert not node_package_dir.exists()
