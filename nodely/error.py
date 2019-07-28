1f0714788a8785c54a2555b79ef9b8f9e162fa23
Stefan Zimmermann <user@zimmermann.co>
2019-07-28 15:19:27 +0200

    NodeCommandError: Fix super() calls for Python 2.7

--- nodely/error.py
+++ nodely/error.py
@@ -9,10 +9,11 @@ class NodeCommandError(CalledProcessError):
     """A Node.js command returned non-zero exit code"""
 
     def __init__(self, returncode, cmd, cwd):
-        super().__init__(returncode=returncode, cmd=cmd)
+        super(NodeCommandError, self).__init__(
+            returncode=returncode, cmd=cmd)
         self.cwd = cwd
         self.args += (cwd, )
 
     def __str__(self):
         return "{} in working directory {!r}".format(
-            super().__str__(), self.cwd)
+            super(NodeCommandError, self).__str__(), self.cwd)
